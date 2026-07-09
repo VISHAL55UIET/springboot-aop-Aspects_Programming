56bdc9100c36671ebb3158217a3be5e72dbd12a6
VISHAL55UIET <vishalsingh962849@gmail.com>
2026-07-09 16:28:12 +0530

    Initial commit: Spring Boot AOP project

---
+++ src/test/java/com/aopApp/AopAppApplication/AopAppApplicationTests.java
@@ -0,0 +1,11 @@
+package com.aopApp.AopAppApplication;
+
+import com.aopApp.AopAppApplication.services.ShipmentService;
+import com.aopApp.AopAppApplication.services.impl.ShipmentServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopAppApplicationTests {
+}
